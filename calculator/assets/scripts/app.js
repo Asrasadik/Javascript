b486806c6e0f208ee5550b142e98576af6f61a38
Asrasadik <47492644+Asrasadik@users.noreply.github.com>
2021-10-06 08:17:33 +0530

    adding event listener for + button

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -1,15 +1,25 @@
 let currentResult = 0;
 
 
-function add (num1 , num2){
-    const result=num1+num2;
-    alert('The sum is '+result);
+function add(){
+    currentResult=currentResult+userInput.value;
+    outputResult(currentResult, calculationDes);
 }
 
-add(1,2);
+addBtn.addEventListener('click',add);
+/*function add (num1 , num2){
+    const result=num1+num2;
+    return result;
+}
 
-currentResult = (currentResult + 10) *3 / 2 -1;
+currentResult = add(1,2); // pure function
+
+//currentResult = (currentResult + 10) *3 / 2 -1;
 
 let calculationDes = `(${currentResult} + 10)*3 / 2 -1`;
 
-outputResult(currentResult, calculationDes);
+//outputResult(currentResult, calculationDes);
+ this runs only once bcz this whole code runs from top to bottom  so its done its work but we want when button
+is clicked it should give output but any time button can be clicked we donno when button gets clicked
+the only thing that runs after button is clicked by browser is add function so we will put this line into add function
+*/
